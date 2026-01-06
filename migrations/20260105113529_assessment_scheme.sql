9c8d59ce8e2e6aa98d84084bad8628ceaff67349
mushroom <mushroom019.coder@gmail.com>
2026-01-06 19:01:48 +0530

    added LKG exam assesments

--- migrations/20260105113529_assessment_scheme.sql
+++ migrations/20260105113529_assessment_scheme.sql
@@ -87,3 +87,75 @@ WHERE g.name = 'Nursery'
   AND et.code = 'CLASS_PER'
   AND t.name IN ('Half Yearly Term', 'Second Term', 'Annual Term')
   AND s.code IN ('CLASS_PERF');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 30
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'WRITT'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('MATHS', 'ENG', 'HIN');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'HW'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('MATHS', 'ENG', 'HIN');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'CN'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('MATHS', 'ENG', 'HIN');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'ORAL_PRAC'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('ENG_ORAL', 'ENG_RHYMES','HIN_ORAL', 'HIN_RHYMES','MATHS_ORAL');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'ACTIVITY'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('WELL_DRESSED', 'DRAW','PT', 'WORKSHEET');
+
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = 'LKG'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('First Term', 'Half Yearly Term', 'Second Term', 'Annual Term')
+  AND s.code IN ('CLASS_PERF');
