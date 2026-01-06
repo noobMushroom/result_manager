757b6569ffa45a8a2f8ab6cadb9694a6f8216190
mushroom <mushroom019.coder@gmail.com>
2026-01-06 22:12:25 +0530

    added assesments upto class 8th

--- migrations/20260105113529_assessment_scheme.sql
+++ migrations/20260105113529_assessment_scheme.sql
@@ -1140,3 +1140,488 @@ WHERE g.name = '5'
   AND t.name IN ('Second Term')
   AND s.code IN ('DRAW', 'PT');
 
+
+-- Class 6--
+-- First term
+-- Unit test
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- half yearly exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 1
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Half yearly
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- second term
+-- Unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Annual Exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Annual
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '6'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
+
+
+-- Class 7--
+-- First term
+-- Unit test
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- half yearly exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 1
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Half yearly
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- second term
+-- Unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Annual Exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Annual
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '7'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
+
+
+-- Class 8--
+-- First term
+-- Unit test
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- half yearly exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 1
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'UT'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Half yearly
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'HALF_YEARLY'
+  AND t.name IN ('First Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- second term
+-- Unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 20
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Copy work
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'COPY_WORK'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Class performance
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 10
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'CLASS_PER'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- Annual Exam (written)
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type, max_marks)
+SELECT g.id, e.id, s.id, 'MARKS', 60
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('ENG_LIT', 'ENG_LANG', 'HIN', 'SANS', 'MATHS', 'SCI_BIO', 'SCI_PHY_CHEM', 'GEO', 'HIST_CIV', 'COMP', 'GK');
+
+-- First term grades
+-- unit test - 2
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'UT'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
+
+-- Annual
+INSERT INTO assessment_scheme (grade_id, exam_id, subject_id, evaluation_type)
+SELECT g.id, e.id, s.id, 'GRADE'
+FROM grades g
+JOIN exams e ON true
+JOIN exam_types et ON e.exam_type_id = et.id
+JOIN terms t ON e.term_id = t.id
+JOIN subjects s ON true
+WHERE g.name = '8'
+  AND et.code = 'ANNUAL'
+  AND t.name IN ('Second Term')
+  AND s.code IN ('DRAW', 'PT');
